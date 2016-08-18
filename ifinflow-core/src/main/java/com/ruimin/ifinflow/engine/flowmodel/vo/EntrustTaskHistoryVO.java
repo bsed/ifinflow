b21c3369c4b51c08b3574038eb5ef2c5166299bf
d0f <284178359@qq.com>
2016-08-18 13:41:23 +0800

    ifinflow engine EntrustTaskHistory

--- ifinflow-core/src/main/java/com/ruimin/ifinflow/engine/flowmodel/vo/EntrustTaskHistoryVO.java
+++ ifinflow-core/src/main/java/com/ruimin/ifinflow/engine/flowmodel/vo/EntrustTaskHistoryVO.java
@@ -1,48 +1,35 @@
-/*    */ package com.ruimin.ifinflow.engine.flowmodel.vo;
-/*    */ 
-/*    */ import java.io.Serializable;
-/*    */ import java.util.Date;
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ public class EntrustTaskHistoryVO
-/*    */   implements Serializable
-/*    */ {
-/*    */   private static final long serialVersionUID = 1L;
-/*    */   private String taskId;
-/*    */   private Date operateTime;
-/*    */   private String message;
-/*    */   
-/*    */   public String getTaskId()
-/*    */   {
-/* 20 */     return this.taskId;
-/*    */   }
-/*    */   
-/*    */   public void setTaskId(String taskId) {
-/* 24 */     this.taskId = taskId;
-/*    */   }
-/*    */   
-/*    */   public Date getOperateTime() {
-/* 28 */     return this.operateTime;
-/*    */   }
-/*    */   
-/*    */   public void setOperateTime(Date operateTime) {
-/* 32 */     this.operateTime = operateTime;
-/*    */   }
-/*    */   
-/*    */   public String getMessage() {
-/* 36 */     return this.message;
-/*    */   }
-/*    */   
-/*    */   public void setMessage(String message) {
-/* 40 */     this.message = message;
-/*    */   }
-/*    */ }
+package com.ruimin.ifinflow.engine.flowmodel.vo;
 
+import java.io.Serializable;
+import java.util.Date;
 
-/* Location:              /Users/Jason/Desktop/ifinflow-core.jar!/com/ruimin/ifinflow/engine/flowmodel/vo/EntrustTaskHistoryVO.class
- * Java compiler version: 5 (49.0)
- * JD-Core Version:       0.7.1
- */
+public class EntrustTaskHistoryVO implements Serializable {
+	private static final long serialVersionUID = 1L;
+	private String taskId;
+	private Date operateTime;
+	private String message;
+
+	public String getTaskId() {
+		return this.taskId;
+	}
+
+	public void setTaskId(String taskId) {
+		this.taskId = taskId;
+	}
+
+	public Date getOperateTime() {
+		return this.operateTime;
+	}
+
+	public void setOperateTime(Date operateTime) {
+		this.operateTime = operateTime;
+	}
+
+	public String getMessage() {
+		return this.message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+}
