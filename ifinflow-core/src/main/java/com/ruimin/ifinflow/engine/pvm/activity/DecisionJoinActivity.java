ace6f19d667cb3ad2da0cada4c5dee7138322fab
d0f <284178359@qq.com>
2016-09-01 09:17:30 +0800

    ifinflow decision join activity

---
+++ ifinflow-core/src/main/java/com/ruimin/ifinflow/engine/pvm/activity/DecisionJoinActivity.java
@@ -0,0 +1,20 @@
+package com.ruimin.ifinflow.engine.pvm.activity;
+
+import java.util.Map;
+import org.jbpm.api.activity.ActivityExecution;
+import org.jbpm.api.activity.ExternalActivityBehaviour;
+
+public class DecisionJoinActivity
+  implements ExternalActivityBehaviour
+{
+  private static final long serialVersionUID = 1L;
+  
+  public void execute(ActivityExecution execution)
+    throws Exception
+  {}
+  
+  public void signal(ActivityExecution execution, String signalName, Map<String, ?> parameters)
+    throws Exception
+  {}
+}
+
