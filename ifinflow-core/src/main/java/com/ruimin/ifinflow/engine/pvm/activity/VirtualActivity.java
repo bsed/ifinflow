3958a4d592caa0611dea93fdc854e2c664ec5f15
d0f <284178359@qq.com>
2016-09-05 17:54:22 +0800

    ifinflow

--- ifinflow-core/src/main/java/com/ruimin/ifinflow/engine/pvm/activity/VirtualActivity.java
+++ ifinflow-core/src/main/java/com/ruimin/ifinflow/engine/pvm/activity/VirtualActivity.java
@@ -4,22 +4,13 @@ import java.util.Map;
 import org.jbpm.api.activity.ActivityExecution;
 import org.jbpm.api.activity.ExternalActivityBehaviour;
 
-public class VirtualActivity
-  implements ExternalActivityBehaviour
-{
-  private static final long serialVersionUID = 1L;
-  
-  public void execute(ActivityExecution execution)
-    throws Exception
-  {}
-  
-  public void signal(ActivityExecution execution, String signalName, Map<String, ?> parameters)
-    throws Exception
-  {}
+public class VirtualActivity implements ExternalActivityBehaviour {
+	private static final long serialVersionUID = 1L;
+
+	public void execute(ActivityExecution execution) throws Exception {
+	}
+
+	public void signal(ActivityExecution execution, String signalName,
+			Map<String, ?> parameters) throws Exception {
+	}
 }
-
-
-/* Location:              /Users/Jason/Desktop/ifinflow-core.jar!/com/ruimin/ifinflow/engine/pvm/activity/VirtualActivity.class
- * Java compiler version: 5 (49.0)
- * JD-Core Version:       0.7.1
- */
